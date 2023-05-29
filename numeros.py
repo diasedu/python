4ab0442030a3ce80e527d31b1af439c87ea67f11
diasedu <eduardodias830@gmail.com>
2023-05-29 01:05:49 -0300

    first python commit

---
+++ numeros.py
@@ -0,0 +1,24 @@
+# Tipos de números em python:
+
+# 1 -> int
+# 1.0 -> float
+# 1+2J -> complexo
+
+# Gerando números por meio de funções embutidas:
+
+print(int(1.0))
+
+print(int('9'))
+
+print(float(1))
+
+print(float('9.2'))
+
+print(float('-inf'))
+
+print(float('nan'))
+
+print(complex(1, 2))
+
+
+#
---
+++ operadores_aritmeticos.py
@@ -0,0 +1,8 @@
+# x  + y
+# x - y
+# x / y -> divisão em ponto flutuante
+# x // y -> divisão que descarta a parte fracionada
+# x * y
+# x % y -> resto
+# -x -> negação
+# x ** y -> potência
