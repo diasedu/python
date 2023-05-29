dfee37779ec91668ecbde0066a32fe612dc69429
diasedu <eduardodias830@gmail.com>
2023-05-29 01:35:04 -0300

    study about variables with py examples.

---
+++ variables/var_multiple_values.py
@@ -0,0 +1,25 @@
+x, y, z = "Orange", "Apple", "Banana"
+
+print(x)
+print(y)
+print(z)
+
+# In this session (Line 1 to 5), it's possible to understand that, in Python, we can to assign differents values to differents variables in only one line
+
+x = y = z = "Orange"
+
+print(x)
+print(y)
+print(z)
+
+# Above, it's noticeable that we can to assign a value a differents variables in only line
+
+fruits = ["Apple", "Banana", "Cherry"]
+
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+
+# This above way (line 17 to 23), I'll show you separate a item list in variables 
---
+++ variables/var_names.py
@@ -0,0 +1,20 @@
+# in Python, we can to utilize some ways about how to declare a variable
+# See below:
+
+
+myvar   = "Edu"
+my_var  = "Edu" # Snake Case
+_my_var = "Edu"
+myVar   = "Edu" # Camel Case
+MYVAR   = "Edu"
+myvar2  = "edu"
+MyVar   = "Edu" # Pascal Case
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+# In my opinion, Snake Case (line 6) way it's a better option to utilizate in Python
